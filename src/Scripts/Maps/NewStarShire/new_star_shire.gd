f04a9ad4e68d74e1f3b1c30ca60458a60b976d5e
Matt Matriciano <mattmatriciano@gmail.com>
2026-01-22 14:17:44 -0800

    I have it fading in/out now when showing a station.. I think this is nicer.

--- src/Scripts/Maps/NewStarShire/new_star_shire.gd
+++ src/Scripts/Maps/NewStarShire/new_star_shire.gd
@@ -31,9 +31,9 @@ func _ready() -> void:
 	TechTreeManager.unlock_refinery.connect(unlock_refinery_station)
 	
 	hud.animation_player.play("CloseIn")
-	await get_tree().create_timer(1.0).timeout
-	
+
 	if PlayerStats.can_craft_next_sword():
+		await get_tree().create_timer(1.0).timeout
 		new_sword_unlock_notice()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -163,8 +163,10 @@ func _on_crafting_station_area_body_exited(body: Node2D) -> void:
 func unlock_cooking_station() -> void:
 	camera.player = null
 	GameManager.player_can_move = false
-	var tween : Tween = get_tree().create_tween()
-	await tween.tween_property(camera, "position", cooking_range_position.position, 1.0).finished
+	hud.animation_player.play("FadeInOut")
+	await get_tree().create_timer(0.5).timeout
+	camera.position = cooking_range_position.position
+	await get_tree().create_timer(1.5).timeout
 	hud.animation_player.play("Flash")
 	await get_tree().create_timer(0.5).timeout
 	temp_cooking_range.unlock_station()
@@ -173,14 +175,19 @@ func unlock_cooking_station() -> void:
 	SignalBus.issue_big_notification.emit("Cooking Resources Drop From Monsters!")
 	await get_tree().create_timer(3.0).timeout
 	SignalBus.hide_big_notification.emit()
+	hud.animation_player.play("FadeInOut")
+	await get_tree().create_timer(0.5).timeout
+	camera.position = player.position
 	camera.player = player
 	GameManager.player_can_move = true
 	
 func unlock_refinery_station() -> void:
 	camera.player = null
 	GameManager.player_can_move = false
-	var tween : Tween = get_tree().create_tween()
-	await tween.tween_property(camera, "position", refinery_position.position, 1.0).finished
+	hud.animation_player.play("FadeInOut")
+	await get_tree().create_timer(0.5).timeout
+	camera.position = refinery_position.position
+	await get_tree().create_timer(1.5).timeout
 	hud.animation_player.play("Flash")
 	await get_tree().create_timer(0.5).timeout
 	temp_smelting_station.unlock_station()
@@ -191,17 +198,23 @@ func unlock_refinery_station() -> void:
 	SignalBus.issue_big_notification.emit("Tin and Copper ore can now be mined in the Tower!")
 	await get_tree().create_timer(3.5).timeout
 	SignalBus.hide_big_notification.emit()
+	hud.animation_player.play("FadeInOut")
+	await get_tree().create_timer(0.5).timeout
+	camera.position = player.position
 	camera.player = player
 	GameManager.player_can_move = true
 
 func new_sword_unlock_notice() -> void:
 	camera.player = null
 	GameManager.player_can_move = false
-	var tween : Tween = get_tree().create_tween()
-	await tween.tween_property(camera, "position", sword_crafting_station_position.position, 1.0).finished
+	hud.animation_player.play("FadeInOut")
 	await get_tree().create_timer(0.5).timeout
+	camera.position = sword_crafting_station_position.position
 	SignalBus.issue_big_notification.emit("A New Sword Can Be Unlocked!")
 	await get_tree().create_timer(3.5).timeout
 	SignalBus.hide_big_notification.emit()
+	hud.animation_player.play("FadeInOut")
+	await get_tree().create_timer(0.5).timeout
+	camera.position = player.position
 	camera.player = player
 	GameManager.player_can_move = true
