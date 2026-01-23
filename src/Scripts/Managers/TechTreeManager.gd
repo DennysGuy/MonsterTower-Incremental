514b0fb67f2a9da8e4d0ed6844ce51dd6bc7d7e7
DennysGuy <mattmatriciano@gmail.com>
2026-01-22 20:57:57 -0800

    got checkpoint room going.

--- src/Scripts/Managers/TechTreeManager.gd
+++ src/Scripts/Managers/TechTreeManager.gd
@@ -77,6 +77,7 @@ func increment_upgrade_count() -> void:
 	
 	if current_upgrade_count >= upgrade_count_to_prestige:
 		current_prestige += 1
+		PlayerStats.player_stats["Expedition Time"] += 30
 		upgrade_count_to_prestige += 25
 		update_prestige_tier_label.emit()
 		update_prestige_tier_progress_label.emit()
--- src/Scripts/tower_entrance_map.gd
+++ src/Scripts/tower_entrance_map.gd
@@ -22,6 +22,7 @@ func _process(delta: float) -> void:
 func _on_go_to_floor_button_up() -> void:
 	SignalBus.play_close_out_animation.emit()
 	GameManager.player_can_move = true
+	hide()
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file(stored_entrance_data.scene_path)
 
