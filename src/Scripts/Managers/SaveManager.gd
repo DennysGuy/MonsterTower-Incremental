3c410268b46fb50fd5c452c2e3947a100b9858bf
Matt Matriciano <mattmatriciano@gmail.com>
2026-02-02 14:09:51 -0800

    I can save and load a save file.

--- src/Scripts/Managers/SaveManager.gd
+++ src/Scripts/Managers/SaveManager.gd
@@ -7,14 +7,17 @@ func save_game() -> void:
 	pass
 
 func load_game() -> void:
-	if ResourceLoader.exists(SAVE_PATH):
+	if save_file_exists():
 		current_save_game = ResourceLoader.load(SAVE_PATH, "", ResourceLoader.CACHE_MODE_IGNORE)
 	
 	else:
 		var new_save_game : GameSave = GameSave.new()
 		var result = ResourceSaver.save(new_save_game, SAVE_PATH)
-		if result == OK:
+		if result :
 			print("Game Saved Successfully!")
 		else:
 			print(result)
 			print("SOMETHING HAPPENED!")
+
+func save_file_exists() -> bool:
+	return ResourceLoader.exists(SAVE_PATH)
