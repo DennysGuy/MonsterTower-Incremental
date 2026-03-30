b4702acfe79b6670ab4a4e10ab465974a7d1bd45
DennysGuy <mattmatriciano@gmail.com>
2026-03-29 19:37:40 -0700

    adjusted to 30 second increase for pass upgrade

--- src/Scripts/Managers/TechTreeManager.gd
+++ src/Scripts/Managers/TechTreeManager.gd
@@ -150,7 +150,7 @@ func increment_upgrade_count() -> void:
 	if current_upgrade_count >= upgrade_count_to_prestige:
 		current_prestige += 1
 		PlayerStats.player_stats["Hunt Time"] += 12
-		PlayerStats.player_stats["Expedition Time"] += 20
+		PlayerStats.player_stats["Expedition Time"] += 30
 		upgrade_count_to_prestige += 15
 		current_upgrade_count = 0
 		update_prestige_tier_label.emit()
